dd77ee6086f821d7b1d1d84f97dd159e1181a0ad
SEJUN <pkt369@naver.com>
2024-06-08 17:42:43 +0900

    default commit

---
+++ src/test/java/birdjun/profairmanager/ProfairmanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package birdjun.profairmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProfairmanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
